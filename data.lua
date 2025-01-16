1f7e6519009255adf5dd532ccab970cc1f8ec43a
rjdunlap <rjdunlap@outlook.com>
2025-01-16 15:40:44 -0800

    0.0.2 asteroid fix

--- data.lua
+++ data.lua
@@ -139,6 +139,22 @@ end
 local nauvis = data.raw["planet"]["nauvis"]
 local planet_lib = require("__PlanetsLib__.lib.planet")
 
+local start_astroid_spawn_rate =
+{
+  probability_on_range_chunk =
+  {
+    {position = 0.1, probability = asteroid_util.nauvis_chunks, angle_when_stopped = asteroid_util.chunk_angle},
+    {position = 0.9, probability = asteroid_util.fulgora_chunks, angle_when_stopped = asteroid_util.chunk_angle}
+  },
+  type_ratios =
+  {
+    {position = 0.1, ratios = asteroid_util.nauvis_ratio},
+    {position = 0.9, ratios = asteroid_util.fulgora_ratio},
+  }
+}
+local start_astroid_spawn = asteroid_util.spawn_definitions(start_astroid_spawn_rate, 0.1)
+
+
 local froodara = 
 {
     type = "planet",
@@ -156,7 +172,9 @@ local froodara =
         ["magnetic-field"] = nauvis.surface_properties["magnetic-field"],
         ["day-night-cycle"] = nauvis.surface_properties["day-night-cycle"],
     },
-    map_gen_settings = MapGen_Froodara()
+    map_gen_settings = MapGen_Froodara(),
+    asteroid_spawn_influence = 1,
+    asteroid_spawn_definitions = start_astroid_spawn
 }
 
 froodara.orbit = {
