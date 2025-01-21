86a27ad5cb0812e409c35d87a635fda5afb0357f
rjdunlap <rjdunlap@outlook.com>
2025-01-21 08:17:21 -0800

    0.0.8

--- froodara.lua
+++ froodara.lua
@@ -2,6 +2,8 @@ local utils = require("__any-planet-start__.utils")
 
 data.raw.technology["planet-discovery-vulcanus"].hidden = true
 
+utils.set_prerequisites("tungsten-carbide", nil)
+
 utils.set_trigger("tungsten-carbide", {type = "mine-entity", entity = "big-volcanic-rock"})
 
 utils.set_prerequisites("foundry", {"tungsten-carbide","concrete", "lubricant"})
